c7598bda65e8a6dd05d22fddf935980700f189bc
thomasuebi <uebsi2804@gmail.com>
2022-07-30 16:30:55 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,95 @@
+import React, { useState } from "react"
+import { Launcher } from "popup-chat-react"
+
+const App = () => {
+  const [state, setState] = useState({
+    messageList: [],
+    newMessagesCount: 0,
+    isOpen: false,
+    fileUpload: true,
+  })
+
+  async function onMessageWasSent(message) {
+    setState((state) => ({
+      ...state,
+      messageList: [...state.messageList, message],
+    }))
+    sendMessage("blub")
+  }
+
+  function onFilesSelected(fileList) {
+    const objectURL = window.URL.createObjectURL(fileList[0])
+
+    setState((state) => ({
+      ...state,
+      messageList: [
+        ...state.messageList,
+        {
+          type: "file",
+          author: "me",
+          data: {
+            url: objectURL,
+            fileName: fileList[0].name,
+          },
+        },
+      ],
+    }))
+  }
+
+  function sendMessage(text) {
+    if (text.length > 0) {
+      const newMessagesCount = state.isOpen
+        ? state.newMessagesCount
+        : state.newMessagesCount + 1
+
+      setState((state) => ({
+        ...state,
+        newMessagesCount: newMessagesCount,
+        messageList: [
+          ...state.messageList,
+          {
+            author: "them",
+            type: "text",
+            data: { text },
+          },
+        ],
+      }))
+    }
+  }
+
+  function onClick() {
+    setState((state) => ({
+      ...state,
+      isOpen: !state.isOpen,
+      newMessagesCount: 0,
+    }))
+  }
+
+  return (
+    <div>
+      <Launcher
+        agentProfile={{
+          teamName: "Gemeinde Sonntagberg",
+          // imageUrl:
+          //   "https://a.slack-edge.com/66f9/img/avatars-teams/ava_0001-34.png",
+        }}
+        onMessageWasSent={onMessageWasSent}
+        onFilesSelected={onFilesSelected}
+        messageList={state.messageList}
+        newMessagesCount={state.newMessagesCount}
+        onClick={onClick}
+        isOpen={state.isOpen}
+        showEmoji
+        fileUpload={state.fileUpload}
+        pinMessage={{
+          // imageUrl:
+          //   "https://a.slack-edge.com/66f9/img/avatars-teams/ava_0001-34.png",
+          title: "Deine digitale Unterstützung!",
+          text: "Ich helfe dir mit all deinen Fragen und dabei, Formulare auszufüllen.",
+        }}
+        placeholder='...'
+      />
+    </div>
+  )
+}
+export default App
